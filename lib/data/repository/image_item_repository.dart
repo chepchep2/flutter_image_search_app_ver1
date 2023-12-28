8ee5c000e03a365fa0eccad804bd46072d4981db
sangwoo <kajer102@gmail.com>
2023-12-28 16:55:46 +0900

    feat: 2회차 mock데이터 조금 더 추가

--- lib/data/repository/image_item_repository.dart
+++ lib/data/repository/image_item_repository.dart
@@ -2,19 +2,34 @@ import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
 
 class MockImageItemRepository {
   Future<List<ImageItem>> getImageItem(String query) async {
-    return [
-      ImageItem(
-          imageUrl:
-              "https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg",
-          tags: ''),
-      ImageItem(
-          imageUrl:
-              "https://cdn.pixabay.com/photo/2017/09/26/13/39/apples-2788651_150.jpg",
-          tags: ''),
-      ImageItem(
-          imageUrl:
-              "https://cdn.pixabay.com/photo/2015/02/13/00/43/apples-634572_150.jpg",
-          tags: ''),
-    ];
+    await Future.delayed(Duration(seconds: 2));
+
+    if (query == 'apple') {
+      return [
+        ImageItem(
+            imageUrl:
+            "https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg",
+            tags: ''),
+        ImageItem(
+            imageUrl:
+            "https://cdn.pixabay.com/photo/2017/09/26/13/39/apples-2788651_150.jpg",
+            tags: ''),
+        ImageItem(
+            imageUrl:
+            "https://cdn.pixabay.com/photo/2015/02/13/00/43/apples-634572_150.jpg",
+            tags: ''),
+      ];
+    } else {
+      return [
+        ImageItem(
+            imageUrl:
+            "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
+            tags: ''),
+        ImageItem(
+            imageUrl:
+            "https://cdn.pixabay.com/photo/2019/09/21/09/07/banana-4493420_150.jpg",
+            tags: ''),
+      ];
+    }
   }
 }
