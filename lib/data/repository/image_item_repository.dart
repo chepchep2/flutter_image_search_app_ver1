3310ba2da062a38ea9e0c423a30f4633483bad6d
sangwoo <kajer102@gmail.com>
2023-12-28 17:29:32 +0900

    feat: 2회차 실제 데이터를 가져오기 위해 repository 추가

--- lib/data/repository/image_item_repository.dart
+++ lib/data/repository/image_item_repository.dart
@@ -1,6 +1,28 @@
+import 'package:flutter_image_search_app_ver1/data/data_source/pixabay_api.dart';
+import 'package:flutter_image_search_app_ver1/data/mapper/image_mapper.dart';
 import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
 
-class MockImageItemRepository {
+abstract interface class ImageItemRepository {
+  Future<List<ImageItem>> getImageItem(String query);
+}
+
+class PixabayImageRepository implements ImageItemRepository {
+  final _api = PixabayApi();
+
+  @override
+  Future<List<ImageItem>> getImageItem(String query) async {
+    final dto = await _api.getImageResult(query);
+
+    if (dto.hits == null) {
+      return [];
+    }
+
+    return dto.hits!.map((e) => e.toImageItem()).toList();
+  }
+}
+
+class MockImageItemRepository implements ImageItemRepository {
+  @override
   Future<List<ImageItem>> getImageItem(String query) async {
     await Future.delayed(Duration(seconds: 2));
 
@@ -8,26 +30,26 @@ class MockImageItemRepository {
       return [
         ImageItem(
             imageUrl:
-            "https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg",
+                "https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg",
             tags: ''),
         ImageItem(
             imageUrl:
-            "https://cdn.pixabay.com/photo/2017/09/26/13/39/apples-2788651_150.jpg",
+                "https://cdn.pixabay.com/photo/2017/09/26/13/39/apples-2788651_150.jpg",
             tags: ''),
         ImageItem(
             imageUrl:
-            "https://cdn.pixabay.com/photo/2015/02/13/00/43/apples-634572_150.jpg",
+                "https://cdn.pixabay.com/photo/2015/02/13/00/43/apples-634572_150.jpg",
             tags: ''),
       ];
     } else {
       return [
         ImageItem(
             imageUrl:
-            "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
+                "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
             tags: ''),
         ImageItem(
             imageUrl:
-            "https://cdn.pixabay.com/photo/2019/09/21/09/07/banana-4493420_150.jpg",
+                "https://cdn.pixabay.com/photo/2019/09/21/09/07/banana-4493420_150.jpg",
             tags: ''),
       ];
     }
