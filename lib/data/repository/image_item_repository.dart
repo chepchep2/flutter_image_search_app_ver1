7eaa2e739a2384e756bc0651d69392c507854c42
sangwoo <kajer102@gmail.com>
2023-12-28 16:46:40 +0900

    feat: 2회차 목데이터가 있는 목레파지토리 생성

---
+++ lib/data/repository/image_item_repository.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+
+class MockImageItemRepository {
+  Future<List<ImageItem>> getImageItem(String query) async {
+    return [
+      ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg",
+          tags: ''),
+      ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2017/09/26/13/39/apples-2788651_150.jpg",
+          tags: ''),
+      ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2015/02/13/00/43/apples-634572_150.jpg",
+          tags: ''),
+    ];
+  }
+}
