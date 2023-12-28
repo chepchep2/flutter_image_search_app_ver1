5c9887419beeda97daf54f64da037ee631a820b9
sangwoo <kajer102@gmail.com>
2023-12-28 16:54:00 +0900

    feat: 2회차 mock데이터를 불러와 searchImage 메서드 추가 후 아이콘 버튼에 추가

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,10 +1,25 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+import 'package:flutter_image_search_app_ver1/data/repository/image_item_repository.dart';
 import 'package:flutter_image_search_app_ver1/ui/widget/image_item_widget.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final repository = MockImageItemRepository();
+
+  List<ImageItem> imageItems = [];
+
+  Future<void> searchImage(String query) async {
+    imageItems = await repository.getImageItem(query);
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +43,9 @@ class MainScreen extends StatelessWidget {
                   ),
                   hintText: 'search',
                   suffixIcon: IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      searchImage('apple');
+                    },
                     icon: const Icon(
                       Icons.search,
                       color: Colors.red,
@@ -39,13 +56,9 @@ class MainScreen extends StatelessWidget {
               const SizedBox(height: 24),
               Expanded(
                 child: GridView.builder(
-                  itemCount: 10,
+                  itemCount: imageItems.length,
                   itemBuilder: (context, index) {
-                    final imageItem = ImageItem(
-                        imageUrl:
-                            'https://img.khan.co.kr/news/2023/01/02/news-p.v1.20230102.1f95577a65fc42a79ae7f990b39e7c21_P1.png',
-                        tags: 'apple');
-
+                    final imageItem = imageItems[index];
                     return ImageItemWidget(
                       imageItem: imageItem,
                     );
