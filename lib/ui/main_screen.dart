85c0775773677cb35677f96fe80acf13e624150d
sangwoo <kajer102@gmail.com>
2023-12-29 12:26:08 +0900

    feat: 3회차 UI 완성

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,93 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
-import 'package:flutter_image_search_app_ver1/data/repository/image_item_repository.dart';
 import 'package:flutter_image_search_app_ver1/ui/widget/image_item_widget.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  // final repository = MockImageItemRepository();
-  final repository = PixabayImageRepository();
-  final textController = TextEditingController();
-
-  List<ImageItem> imageItems = [];
-  bool isLoading = false;
-
-  Future<void> searchImage(String query) async {
-    setState(() {
-      isLoading = true;
-    });
-
-    imageItems = await repository.getImageItem(query);
-
-    setState(() {
-      isLoading = false;
-    });
-  }
-
-  @override
-  void dispose() {
-    textController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(24.0),
+          padding: const EdgeInsets.all(32.0),
           child: Column(
             children: [
               TextField(
-                controller: textController,
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide:
-                        const BorderSide(color: Colors.yellow, width: 5),
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(color: Colors.red, width: 4),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide:
-                        const BorderSide(color: Colors.yellow, width: 5),
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(color: Colors.red, width: 4),
                   ),
                   hintText: 'search',
                   suffixIcon: IconButton(
-                    onPressed: () {
-                      searchImage(textController.text);
-                    },
-                    icon: const Icon(
-                      Icons.search,
-                      color: Colors.red,
-                    ),
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
                   ),
                 ),
               ),
-              const SizedBox(height: 24),
-              isLoading
-                  ? const CircularProgressIndicator()
-                  : Expanded(
-                      child: GridView.builder(
-                        itemCount: imageItems.length,
-                        itemBuilder: (context, index) {
-                          final imageItem = imageItems[index];
-                          return ImageItemWidget(
-                            imageItem: imageItem,
-                          );
-                        },
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisSpacing: 32,
-                                mainAxisSpacing: 32,
-                                crossAxisCount: 2),
-                      ),
-                    ),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 10,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    final imageItem = ImageItem(
+                      imageUrl:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS43LjalHIciOPHaS2ZS9VIqtkrSEmaoynXg&usqp=CAU',
+                      tags: 'apple',
+                    );
+
+                    return ImageItemWidget(
+                      imageItem: imageItem,
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
--- lib/ui/widget/image_item_widget.dart
+++ lib/ui/widget/image_item_widget.dart
@@ -1,19 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
 
 class ImageItemWidget extends StatelessWidget {
   final ImageItem imageItem;
 
-  const ImageItemWidget({
-    super.key,
+  const ImageItemWidget({super.key,
     required this.imageItem,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      imageItem.imageUrl,
-      fit: BoxFit.cover,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Image.network(
+        imageItem.imageUrl,
+        fit: BoxFit.cover,
+      ),
     );
   }
+
+
 }
