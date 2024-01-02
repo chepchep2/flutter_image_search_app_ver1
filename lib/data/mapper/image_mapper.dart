6dbc45c22acee8665acf68308fcb97b8a6039c85
sangwoo <kajer102@gmail.com>
2024-01-02 22:11:47 +0900

    feature: 5회차 mapper 작성

---
+++ lib/data/mapper/image_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_image_search_app_ver1/data/dto/pixaybay_dto.dart';
+import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+
+extension DtoToModel on Hits {
+  ImageItem toImageItem() {
+    return ImageItem(
+      imageUrl: previewURL ??
+          'https://pbs.twimg.com/media/GAALXGibsAEI8cD?format=jpg&name=large',
+      tags: tags ?? '',
+    );
+  }
+}
