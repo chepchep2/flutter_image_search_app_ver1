b1ea33f6efd371c95afb4ed5f5c8ad36ab15379e
sangwoo <kajer102@gmail.com>
2024-01-04 22:09:30 +0900

    feature: 7회차 ui완성 시작

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,17 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+import 'package:flutter_image_search_app_ver1/ui/widget/image_item_widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: Text('ttt'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide:
+                        const BorderSide(width: 5, color: Colors.yellow),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide:
+                        const BorderSide(width: 5, color: Colors.yellow),
+                  ),
+                  hintText: '검색',
+                  hintStyle: const TextStyle(color: Colors.red),
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    final imageItem = ImageItem(
+                      imageUrl:
+                          'https://img.taling.me/Content/Uploads/Cover/9a9f123ad24707b13d0735444e0604c88059476a.png',
+                      tags: 'flutter',
+                    );
+                    return ImageItemWidget(
+                      imageItem: imageItem,
+                    );
+                  },
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
