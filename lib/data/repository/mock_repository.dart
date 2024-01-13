a5b162a4aa66a9306fa51a6f1e18493f6a6bdd0c
sangwoo <kajer102@gmail.com>
2024-01-13 16:38:57 +0900

    feature: 16회차 mock 데이터 작동 성공

--- lib/data/repository/mock_repository.dart
+++ lib/data/repository/mock_repository.dart
@@ -4,22 +4,42 @@ import 'package:flutter_image_search_app_ver1/data/repository/image_repository.d
 class MockRepository implements ImageRepository {
   @override
   Future<List<ImageItem>> getImageItem(String query) async {
-    return [
-      ImageItem(
-        imageUrl:
-            "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
-        tags: '',
-      ),
-      ImageItem(
-        imageUrl:
-        "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
-        tags: '',
-      ),
-      ImageItem(
-        imageUrl:
-        "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
-        tags: '',
-      ),
-    ];
+    if (query == 'banana') {
+      return [
+        ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
+          tags: '',
+        ),
+        ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
+          tags: '',
+        ),
+        ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
+          tags: '',
+        ),
+      ];
+    } else {
+      return [
+        ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2019/09/21/09/07/banana-4493420_150.jpg",
+          tags: '',
+        ),
+        ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2016/11/22/23/04/citrus-fruits-1851077_150.jpg",
+          tags: '',
+        ),
+        ImageItem(
+          imageUrl:
+              "https://cdn.pixabay.com/photo/2016/09/03/20/48/bananas-1642706_150.jpg",
+          tags: '',
+        ),
+      ];
+    }
   }
 }
