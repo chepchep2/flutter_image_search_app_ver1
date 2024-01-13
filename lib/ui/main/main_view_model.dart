0f9528d4ec11a7fb261e2c8529acc7e565e466b2
sangwoo <kajer102@gmail.com>
2024-01-13 16:45:03 +0900

    feature: 16회차 실제 데이터 불러오기 성공 get_it, go_route 추가 시작

--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
 import 'package:flutter_image_search_app_ver1/data/repository/image_repository.dart';
 import 'package:flutter_image_search_app_ver1/data/repository/mock_repository.dart';
+import 'package:flutter_image_search_app_ver1/data/repository/pixabay_repository.dart';
 
 class MainViewModel extends ChangeNotifier {
-  final ImageRepository repository = MockRepository();
+  final ImageRepository repository = PixabayRepository();
 
   List<ImageItem> imageItems = [];
 
