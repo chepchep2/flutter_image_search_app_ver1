d27d047bd863e693109e2cb9cfd4e8ef015681d8
sangwoo <kajer102@gmail.com>
2024-01-14 21:16:52 +0900

    feature: 17회차 모델클래스, datasource, mapper, dto, 레포지토리 작성

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -6,7 +6,32 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              labelText: 'search',
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          GridView.builder(itemCount: 10,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                return
+              })
+        ],
+      ),
     );
   }
 }
