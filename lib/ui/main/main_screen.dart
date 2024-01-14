43e05ae8c868581a1a0fa5d0350f84ab629dcdce
sangwoo <kajer102@gmail.com>
2024-01-14 21:30:21 +0900

    feature: 17회차 완성

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,36 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+import 'package:flutter_image_search_app_ver1/ui/main/main_view_model.dart';
+import 'package:flutter_image_search_app_ver1/ui/widget/image_item_widget.dart';
+import 'package:provider/provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final textController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+
     return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: textController,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  labelText: 'search',
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      viewModel.searchImage(textController.text);
+                    },
+                    icon: const Icon(Icons.search),
+                  ),
+                ),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              labelText: 'search',
-              suffixIcon: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.search),
-              ),
-            ),
+              const SizedBox(height: 20),
+              viewModel.isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : Expanded(
+                      child: GridView.builder(
+                          itemCount: viewModel.imageItems.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2),
+                          itemBuilder: (context, index) {
+                            final imageItem = viewModel.imageItems[index];
+
+                            return ImageItemWidget(imageItem: imageItem);
+                          }),
+                    )
+            ],
           ),
-          SizedBox(height: 20),
-          GridView.builder(itemCount: 10,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2),
-              itemBuilder: (context, index) {
-                return
-              })
-        ],
+        ),
       ),
     );
   }
