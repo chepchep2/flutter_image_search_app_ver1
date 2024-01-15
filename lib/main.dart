3d8a06ed2b8727edfc06874442cd8635cede1092
sangwoo <kajer102@gmail.com>
2024-01-15 22:42:17 +0900

    feature: 18회차 시작

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_image_search_app_ver1/di/di_setup.dart';
-import 'package:flutter_image_search_app_ver1/routes.dart';
 
-import 'package:flutter_image_search_app_ver1/ui/main/main_screen.dart';
 
 void main() {
-  diSetup();
+
   runApp(const MyApp());
 }
 
@@ -15,8 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
