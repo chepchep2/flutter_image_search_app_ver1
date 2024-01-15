4779a3fb97f63205b2db3db7772ca9a0d71a17f5
sangwoo <kajer102@gmail.com>
2024-01-15 22:48:12 +0900

    feature: 18회차 datasource, dto, mapper 작성

---
+++ lib/data/data_source/pixabay_api.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+
+import 'package:flutter_image_search_app_ver1/data/dto/pixabay_dto.dart';
+import 'package:http/http.dart' as http;
+
+class PixabayApi {
+  Future<PixabayDto> getImageResult(String query) async {
+    final response = await http.get(Uri.parse('https://pixabay.com/api/?key=41490894-18bca8c98867b05cc6159e7e7&q=$query&image_type=photo'));
+
+    return PixabayDto.fromJson(jsonDecode(response.body));
+  }
+}
