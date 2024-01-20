78a82821a92fd4763160c44f556858143786dc11
sangwoo <kajer102@gmail.com>
2024-01-21 08:37:38 +0900

    feat: 22회차 ui뼈대 완성

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+import 'package:flutter_image_search_app_ver1/ui/widget/image_item_widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -6,10 +8,48 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          TextField()
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'search',
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 10,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 24,
+                    crossAxisSpacing: 24,
+                  ),
+                  itemBuilder: (context, index) {
+                    final imageItem = ImageItem(
+                        imageUrl:
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrtTyaoWb-207D8wu3iJS_46KnxyhTyvYEPg&usqp=CAU',
+                        tags: '');
+
+                    return ImageItemWidget(imageItem: imageItem);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
