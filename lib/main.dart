29ae317fac7fd84b8669867617ba463b9d7d0851
sangwoo <kajer102@gmail.com>
2024-01-22 20:36:03 +0900

    feat: 23회차 시작

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_image_search_app_ver1/di/di_setup.dart';
-import 'package:flutter_image_search_app_ver1/routes.dart';
 import 'package:flutter_image_search_app_ver1/ui/main/main_screen.dart';
-import 'package:flutter_image_search_app_ver1/ui/main/main_view_model.dart';
-import 'package:provider/provider.dart';
+
 
 void main() {
-  diSetup();
   runApp(const MyApp());
 }
 
@@ -15,14 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: MainScreen(),
     );
   }
 }
