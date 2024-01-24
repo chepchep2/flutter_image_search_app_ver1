b735cbdaacabf47f1e2309f4322ba796c883a5b7
sangwoo <kajer102@gmail.com>
2024-01-24 22:02:37 +0900

    feat: 25회차 dto, mapper, repository, datasource 작성

---
+++ lib/ui/main/main_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/data/repository/image_repository.dart';
+
+class MainViewModel extends ChangeNotifier {
+  final ImageRepository
+}
