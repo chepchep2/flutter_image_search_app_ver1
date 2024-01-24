2698c0a4aa64e0a04b250c7002873ea80c8eeee7
sangwoo <kajer102@gmail.com>
2024-01-24 21:46:51 +0900

    feat: 25회차 router 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
