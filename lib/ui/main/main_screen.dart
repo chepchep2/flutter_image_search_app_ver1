05780daa7c539510877c638cb295b806ae78783d
sangwoo <kajer102@gmail.com>
2024-01-31 00:27:44 +0900

    feat: 29회차 ui 기본 뼈대 작성

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final viewModel = context.watch<MainViewModel>();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+              TextField(),
+              SizedBox(height: 24),
+              viewModel.isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : Expanded(
+                      child: GridView.builder(
+                        itemCount: viewModel.imageItems.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 20),
+                        itemBuilder: (context, index) {
+                          final imageItem = viewModel.imageItems[index];
+
+                          return;
+                        },
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/ui/main/widget/image_item_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+
+class ImageItemWidget extends StatelessWidget {
+  final ImageItem imageItem;
+
+  const ImageItemWidget({
+    super.key,
+    required this.imageItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
