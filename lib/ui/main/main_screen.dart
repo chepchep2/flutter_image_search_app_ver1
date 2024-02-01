a01084c23db42168b13751722d333f978b43fbca
sangwoo <kajer102@gmail.com>
2024-02-01 11:12:21 +0900

    feat: 30회차 정상 작동

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+import 'package:flutter_image_search_app_ver1/ui/main/main_view_model.dart';
 import 'package:flutter_image_search_app_ver1/ui/main/widget/image_item_widget.dart';
+import 'package:provider/provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -14,6 +31,7 @@ class MainScreen extends StatelessWidget {
           child: Column(
             children: [
               TextField(
+                controller: textController,
                 decoration: InputDecoration(
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
@@ -23,31 +41,31 @@ class MainScreen extends StatelessWidget {
                   ),
                   hintText: 'search',
                   suffixIcon: IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      viewModel.searchImage(textController.text);
+                    },
                     icon: Icon(Icons.search),
                   ),
                 ),
               ),
               SizedBox(height: 24),
-              Expanded(
-                child: GridView.builder(
-                  itemCount: 10,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 20,
-                    crossAxisSpacing: 20,
-                  ),
-                  itemBuilder: (context, index) {
-                    final imageItem = ImageItem(
-                      imageUrl:
-                          'https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg5NjQ5NTg3MTE4NTQ4MTY1/usatsi_18324070_168388303_lowres.jpg',
-                      tags: '',
-                    );
+              viewModel.isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : Expanded(
+                      child: GridView.builder(
+                        itemCount: viewModel.imageItems.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 20,
+                          crossAxisSpacing: 20,
+                        ),
+                        itemBuilder: (context, index) {
+                          final imageItem = viewModel.imageItems[index];
 
-                    return ImageItemWidget(imageItem: imageItem);
-                  },
-                ),
-              ),
+                          return ImageItemWidget(imageItem: imageItem);
+                        },
+                      ),
+                    ),
             ],
           ),
         ),
