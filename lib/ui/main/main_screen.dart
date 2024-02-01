06bf24516dda00ec0017a664db3a6f384273c686
sangwoo <kajer102@gmail.com>
2024-02-01 10:54:35 +0900

    feat: 30회차 모델 클래스, ui에서 사용할 위젯 작성

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+import 'package:flutter_image_search_app_ver1/ui/main/widget/image_item_widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -31,9 +33,18 @@ class MainScreen extends StatelessWidget {
                 child: GridView.builder(
                   itemCount: 10,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 20,
+                  ),
                   itemBuilder: (context, index) {
-                    return;
+                    final imageItem = ImageItem(
+                      imageUrl:
+                          'https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg5NjQ5NTg3MTE4NTQ4MTY1/usatsi_18324070_168388303_lowres.jpg',
+                      tags: '',
+                    );
+
+                    return ImageItemWidget(imageItem: imageItem);
                   },
                 ),
               ),
