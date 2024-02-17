87ee4546a4ac7dfca99fa3609400a6e774de441f
sangwoo <kajer102@gmail.com>
2024-02-17 14:06:34 +0900

    feat: 31회차 정상작동

---
+++ lib/data/repository/image_repository.dart
@@ -0,0 +1,7 @@
+
+
+import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
+
+abstract interface class ImageRepository {
+  Future<List<ImageItem>> getImageItem(String query);
+}
--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_image_search_app_ver1/data/model/image_item.dart';
 import 'package:flutter_image_search_app_ver1/ui/main/image_item_widget.dart';
+import 'package:flutter_image_search_app_ver1/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -14,6 +30,7 @@ class MainScreen extends StatelessWidget {
           child: Column(
             children: [
               TextField(
+                controller: textController,
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
@@ -23,23 +40,27 @@ class MainScreen extends StatelessWidget {
                   ),
                   hintText: 'search',
                   suffixIcon: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.search),
+                    onPressed: () {
+                      viewModel.searchImage(textController.text);
+                    },
+                    icon: const Icon(Icons.search),
                   ),
                 ),
               ),
-              Expanded(
-                child: GridView.builder(
-                  itemCount: 10,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: (context, index) {
-                    final imageItem = ImageItem(imageUrl: 'asd', tags: '');
+              viewModel.isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : Expanded(
+                      child: GridView.builder(
+                        itemCount: viewModel.imageItems.length,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                        itemBuilder: (context, index) {
+                          final imageItem = viewModel.imageItems[index];
 
-                    return ImageItemWidget(imageItem: imageItem);
-                  },
-                ),
-              ),
+                          return ImageItemWidget(imageItem: imageItem);
+                        },
+                      ),
+                    ),
             ],
           ),
         ),
