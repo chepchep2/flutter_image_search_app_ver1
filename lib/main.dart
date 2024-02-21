b281f4f877c6373f360f3888353b2ff7975271dd
sangwoo <kajer102@gmail.com>
2024-02-21 16:49:11 +0900

    feat: 32회차 시작

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_image_search_app_ver1/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
